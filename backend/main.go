ce8e0aca22b325beb046b9b5de715808920c2c85
Marc Dwyer <marcdwyer1738@gmail.com>
2019-03-22 20:21:58 -0500

    init commit

---
+++ backend/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+var Results []Newlive
+var wg sync.WaitGroup
+var mykey string
+
+func init() {
+	fmt.Println(runtime.NumCPU())
+	ky := &mykey
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	*ky = os.Getenv("KEY")
+}
+
+func main() {
+	fmt.Println("Server Started...")
+	hub := newHub()
+	go hub.run()
+	go Listener(hub)
+	go getStreamData()
+	go func() {
+		pollInterval := 10
+
+		timerCh := time.Tick(time.Duration(pollInterval) * time.Minute)
+		for range timerCh {
+			go getStreamData()
+		}
+	}()
+	http.HandleFunc("/sockets/", func(w http.ResponseWriter, r *http.Request) {
+		SocketMe(hub, w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
---
+++ backend/youtube.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type TwitchResponse struct {
+	Stream struct {
+		ID          *int       `json:"id"`
+		Game        *string    `json:"game"`
+		Viewers     *int       `json:"viewers"`
+		VideoHeight *int       `json:"videoheight"`
+		AverageFps  *int       `json:"averggefps"`
+		Delay       *int       `json:"delay"`
+		CreatedAt   *time.Time `json:"createdAt"`
+		IsPlaylist  *bool      `json:"isPlaylist"`
+		Preview     struct {
+			Small    *string `json:"small"`
+			Medium   *string `json:"medium"`
+			Large    *string `json:"large"`
+			Template *string `json:"template"`
+		}
+		Channel struct {
+			Mature                       bool        `json:"mature"`
+			Status                       *string     `json:"status"`
+			BroadcasterLanguage          string      `json:"broadcaster_language"`
+			DisplayName                  string      `json:"display_name"`
+			Game                         string      `json:"game"`
+			Language                     string      `json:"language"`
+			ID                           int         `json:"_id"`
+			Name                         string      `json:"name"`
+			CreatedAt                    time.Time   `json:"created_at"`
+			UpdatedAt                    time.Time   `json:"updated_at"`
+			Partner                      bool        `json:"partner"`
+			Logo                         string      `json:"logo"`
+			VideoBanner                  string      `json:"video_banner"`
+			ProfileBanner                string      `json:"profile_banner"`
+			ProfileBannerBackgroundColor interface{} `json:"profile_banner_background_color"`
+			URL                          string      `json:"url"`
+			Views                        int         `json:"views"`
+			Followers                    int         `json:"followers"`
+		} `json:"channel"`
+	} `json:"stream"`
+}
+
+type Newlive struct {
+	Name        *string    `json:"name"`
+	ImageID     *string    `json:"imageId"`
+	ChannelID   *string    `json:"channelId"`
+	Title       *string    `json:"title"`
+	Description *string    `json:"description,omitempty"`
+	Viewers     *int       `json:"viewers"`
+	Likes       *string    `json:"likes,omitempty"`
+	Dislikes    *string    `json:"dislikes,omitempty"`
+	VideoID     *string    `json:"videoId, omitempty"`
+	Thumbnail   Thumbnails `json:"thumbnails"`
+	DisplayName *string    `json:"displayName,omitempty"`
+	IsPlaying   *string    `json:"isPlaying,omitempty"`
+	Mature      *bool      `json:"mature, omitempty"`
+	Type        string     `json:"type"`
+}
+type TheStreamers map[string]Streamer
+
+var streamers = []Streamer{
+	{Name: "IcePoseidon", ChannelId: "UCv9Edl_WbtbPeURPtFDo-uA", ImageID: "ice.jpg", Type: "youtube"},
+	{Name: "Hyphonix", ChannelId: "UCaFpm67qMk1W1wJkFhGXucA", ImageID: "hyphonix.jpg", Type: "youtube"},
+	{Name: "Gary", ChannelId: "UCvxSwu13u1wWyROPlCH-MZg", ImageID: "gary.jpg", Type: "youtube"},
+	{Name: "Cxnews", ChannelId: "UCStEQ9BjMLjHTHLNA6cY9vg", ImageID: "cxnews.jpg", Type: "youtube"},
+	{Name: "Juan Bagnell", ChannelId: "UCvhnYODy6WQ0mw_zi3V1h0g", ImageID: "juan.jpg", Type: "youtube"},
+	{Name: "Coding Train", ChannelId: "UCvjgXvBlbQiydffZU7m1_aw", ImageID: "coding.jpg", Type: "youtube"},
+	{Name: "Joe Rogan Podcast", ChannelId: "UCzQUP1qoWDoEbmsQxvdjxgQ", ImageID: "joe.jpg", Type: "youtube"},
+	{Name: "Mixhound", ChannelId: "UC_jxnWLGJ2eQK4en3UblKEw", ImageID: "mix.jpg", Type: "youtube"},
+	{Name: "Destiny", Type: "twitch", ImageID: "destiny.jpg"},
+	{Name: "Richardlewisreports", Type: "twitch", ImageID: "richardlewis.jpeg"},
+	{Name: "Cjayride", Type: "twitch", ImageID: "cjayride.jpg"},
+	{Name: "Hitch", Type: "twitch", ImageID: "hitch.jpg"},
+	{Name: "Rajjpatel", Type: "twitch", ImageID: "rajjpatel.jpg"},
+	{Name: "TrainwrecksTv", Type: "twitch", ImageID: "trainwreckstv.jpg"},
+	{Name: "GreekGodx", Type: "twitch", ImageID: "greekgodx.jpeg"},
+	{Name: "EsfandTV", Type: "twitch", ImageID: "esfandtv.jpeg"},
+	{Name: "Alecludford", Type: "twitch", ImageID: "alecludford.jpeg"},
+	{Name: "Knightsinclair", Type: "twitch", ImageID: "knightsinclair.jpg"},
+	{Name: "Dkane", Type: "twitch", ImageID: "dkane.png "},
+	{Name: "Hyphonix", Type: "twitch", ImageID: "hyphonixtwitch.jpeg"},
+	{Name: "staysafetv", Type: "twitch", ImageID: "staysafetv.jpeg"},
+	{Name: "jaycgee", Type: "twitch", ImageID: "jaycgee.jpeg"},
+}
+var streamerObj = TheStreamers{
+	"IcePoseidon":         {Name: "Ice Poseidon", ChannelId: "UCv9Edl_WbtbPeURPtFDo-uA", ImageID: "ice.jpg", Type: "youtube"},
+	"Hyphonix":            {Name: "Hyphonix", ChannelId: "UCaFpm67qMk1W1wJkFhGXucA", ImageID: "hyphonix.jpg", Type: "youtube"},
+	"Gary":                {Name: "Gary", ChannelId: "UCvxSwu13u1wWyROPlCH-MZg", ImageID: "gary.jpg", Type: "youtube"},
+	"Cxnews":              {Name: "Cxnews", ChannelId: "UCStEQ9BjMLjHTHLNA6cY9vg", ImageID: "cxnews.jpg", Type: "youtube"},
+	"Juan Bagnell":        {Name: "Juan Bagnell", ChannelId: "UCvhnYODy6WQ0mw_zi3V1h0g", ImageID: "juan.jpg", Type: "youtube"},
+	"CodingTrain":         {Name: "Coding Train", ChannelId: "UCvjgXvBlbQiydffZU7m1_aw", ImageID: "coding.jpg", Type: "youtube"},
+	"JoeRoganPodcast":     {Name: "Joe Rogan Podcast", ChannelId: "UCzQUP1qoWDoEbmsQxvdjxgQ", ImageID: "joe.jpg", Type: "youtube"},
+	"Mixhound":            {Name: "Mixhound", ChannelId: "UC_jxnWLGJ2eQK4en3UblKEw", ImageID: "mix.jpg", Type: "youtube"},
+	"Destiny":             {Name: "Destiny", Type: "twitch", ImageID: "destiny.jpg"},
+	"Richardlewisreports": {Name: "Richardlewisreports", Type: "twitch", ImageID: "richardlewis.jpeg"},
+	"Cjayride":            {Name: "Cjayride", Type: "twitch", ImageID: "cjayride.jpg"},
+	"Hitch":               {Name: "Hitch", Type: "twitch", ImageID: "hitch.jpg"},
+	"Rajjpatel":           {Name: "Rajjpatel", Type: "twitch", ImageID: "rajjpatel.jpg"},
+	"TrainwrecksTv":       {Name: "TrainwrecksTv", Type: "twitch", ImageID: "trainwreckstv.jpg"},
+	"GreekGodx":           {Name: "GreekGodx", Type: "twitch", ImageID: "greekgodx.jpeg"},
+	"EsfandTV":            {Name: "EsfandTV", Type: "twitch", ImageID: "esfandtv.jpeg"},
+	"Alecludford":         {Name: "Alecludford", Type: "twitch", ImageID: "alecludford.jpeg"},
+	"Knightsinclair":      {Name: "Knightsinclair", Type: "twitch", ImageID: "knightsinclair.jpg"},
+	"Dkane":               {Name: "Dkane", Type: "twitch", ImageID: "dkane.png "},
+	"HyphonixTwitch":      {Name: "Hyphonix", Type: "twitch", ImageID: "hyphonixtwitch.jpeg"},
+	"staysafetv":          {Name: "staysafetv", Type: "twitch", ImageID: "staysafetv.jpeg"},
+}
+var (
+	waiter sync.WaitGroup
+	ch     = make(chan Newlive)
+	isDone = make(chan bool)
+)
+
+func getStreamData() {
+	waiter.Add(len(streamers))
+	for _, s := range streamers {
+		go s.getData()
+	}
+	waiter.Wait()
+	fmt.Println("waiter done")
+	isDone <- true
+}
+func Listener(h *Hub) {
+	payload := []Newlive{}
+	for {
+		select {
+		case data := <-ch:
+			payload = append(payload, data)
+		case <-isDone:
+			sort.Sort(ByViewers(payload))
+			Results = payload
+			payload = nil
+			rz, _ := json.Marshal(Results)
+			h.broadcast <- rz
+		}
+	}
+}
+func (s Streamer) getData() {
+	defer waiter.Done()
+	if s.Type == "youtube" {
+		return
+		url := fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=%v&eventType=live&type=video&key=%v", s.ChannelId, os.Getenv("KEY"))
+		resp, err := http.Get(url)
+		if err != nil || resp.StatusCode != 200 {
+			fmt.Println("youtube req erro")
+			fmt.Println(resp)
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		var streamer Islive
+		json.Unmarshal(body, &streamer)
+		if streamer.PageInfo.TotalResults == 0 {
+			return
+		}
+		streamer.Name = s.Name
+		streamer.ImageID = s.ImageID
+
+		id := streamer.Items[0].ID.VideoID
+		resp, err = http.Get("https://www.googleapis.com/youtube/v3/videos?part=statistics%2C+snippet%2C+liveStreamingDetails&id=" + id + "&key=" + mykey)
+		if err != nil || resp.StatusCode != 200 {
+			fmt.Println(err)
+			return
+		}
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer resp.Body.Close()
+		var live Livestream
+		json.Unmarshal(body, &live)
+		name, err := strconv.Atoi(live.Items[0].LiveStreamingDetails.ConcurrentViewers)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		thumb := Thumbnails{Low: &live.Items[0].Snippet.Thumbnails.High.URL}
+		if len(live.Items[0].Snippet.Thumbnails.Maxres.URL) > 0 {
+			thumb.High = &live.Items[0].Snippet.Thumbnails.Maxres.URL
+		}
+
+		rz := Newlive{
+			Name:        &streamer.Name,
+			ImageID:     &streamer.ImageID,
+			ChannelID:   &live.Items[0].Snippet.ChannelID,
+			Title:       &live.Items[0].Snippet.Title,
+			Description: &live.Items[0].Snippet.Description,
+			Viewers:     &name,
+			Likes:       &live.Items[0].Statistics.LikeCount,
+			Dislikes:    &live.Items[0].Statistics.DislikeCount,
+			VideoID:     &live.Items[0].ID,
+			Thumbnail:   thumb,
+			Type:        "youtube",
+		}
+		ch <- rz
+	} else if s.Type == "twitch" {
+		url := fmt.Sprintf("https://api.twitch.tv/kraken/streams/%v?client_id=%v", s.Name, os.Getenv("TWITCH"))
+		rz, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		body, err := ioutil.ReadAll(rz.Body)
+		defer rz.Body.Close()
+		var res TwitchResponse
+		json.Unmarshal(body, &res)
+		if res.Stream.Channel.Status == nil {
+			return
+		}
+		thumb := Thumbnails{High: res.Stream.Preview.Large, Low: res.Stream.Preview.Medium}
+		result := Newlive{
+			ChannelID:   &res.Stream.Channel.Name,
+			Name:        &res.Stream.Channel.Name,
+			ImageID:     &res.Stream.Channel.Logo,
+			VideoID:     &res.Stream.Channel.Name,
+			Title:       res.Stream.Channel.Status,
+			Viewers:     res.Stream.Viewers,
+			Thumbnail:   thumb,
+			DisplayName: &res.Stream.Channel.DisplayName,
+			IsPlaying:   res.Stream.Game,
+			Mature:      &res.Stream.Channel.Mature,
+			Type:        "twitch",
+		}
+		ch <- result
+	}
+}
